550b03aacac10d66d138d2a7820bebb66cd2eb80
tonycastle <eo057@OA.PNRAD.NET>
2019-06-24 16:56:15 +0200

    added CORS support and routing

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3012
+var cors = require('cors');  //add cors so we can connect to app from gui on different port
 
-app.get('/',(req,res)=>res.send('Hello World'))
+//use cors before we start routing
+app.use(cors());
+//create mock responses to test the gui - we can plug this in to storage later
+//get a profile
+app.get('/profile/:id',(req,res)=>res.json({
+  firstName : 'Tony',
+  lastName : 'Castle',
+  email : 'castleantony6@gmail.com',
+  password : 'pass',
+  address1 : '1 street',
+  address2 : 'sometown',
+  country : 'acountry',
+  profileId : '1'
+}))  
+//update a  profile
+app.post('/profile/update/:id',(req,res)=>res.json(req))
+//create a profile
+app.post('/profile',(req,res)=>res.json(req))
+//add 404 page
+app.use(function (req, res, next) {
+    res.status(404).send("You can't handle that Jelly!!!!")
+  })
 
 app.listen(port, ()=>console.log(`app listening on port ${port}`))
